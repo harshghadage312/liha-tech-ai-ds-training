3fa7147a09556a7e83f133fadc4ffd79370205bf
Harshad Ghadage <hghadge312@gmail.com>
2026-08-08 22:44:51 +0530

    Q05 added

---
+++ Python/Assignment_01/Q05.py
@@ -0,0 +1,6 @@
+#Write a Python program to check whether a given string is a palindrome.
+s=input("enter the string: ")
+if s==s[ : :-1]:
+    print("string is palindrome")
+else:
+    print("string is not palindrome")
