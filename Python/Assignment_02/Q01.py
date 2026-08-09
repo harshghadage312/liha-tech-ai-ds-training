fffe05554ddd2019e58b49d7ec8835ca3a536fca
Harshad Ghadage <hghadge312@gmail.com>
2026-08-09 11:52:02 +0530

    adding if else problem

---
+++ Python/Assignment_02/Q01.py
@@ -0,0 +1,7 @@
+#Write a Python program to find maximum between two numbers.
+num1=int(input("enter the number 1: "))
+num2=int(input("enter the number 2: "))
+if num1> num2:
+    print(num1," is maximum")
+else:
+    print(num2," is maximum")
